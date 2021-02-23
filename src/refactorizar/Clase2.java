ecb7e791164e2db12d3fd33ce59a7610c882cf59
dam <dam@10.100.100.37>
2021-02-23 13:13:50 +0100

    Creacion de rama, etiqueta y clase2

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    String nombre;
+    float precio;
+
+    public Clase2(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    
+}
